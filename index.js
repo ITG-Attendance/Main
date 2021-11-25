a7cd074331a9c15b043bfd38f9694fef1baf66d9
Gold3n <marek@pavlicek.eu>
2021-11-25 11:10:15 +0100

    Nevim co dál

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 function openNav() {
-    document.getElementById("myNav").style.width = "25%";
-    document.getElementById("myNav").style.height = "40%";
+    document.getElementById("myNav").style.width = "350px";
+    document.getElementById("myNav").style.height = "250px";
         
 }
 
 /* Close when someone clicks on the "x" symbol inside the overlay */
 function closeNav() {
-    document.getElementById("myNav").style.width = "0%";
-    document.getElementById("myNav").style.height = "0%";
+    document.getElementById("myNav").style.width = "0px";
+    document.getElementById("myNav").style.height = "50px";
 }
