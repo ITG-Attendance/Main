90f4cfe4dda102dd6d9c4ffa8067cb4ac0a7bb53
Gold3n <marek@pavlicek.eu>
2021-11-25 08:38:26 +0100

    JS je mrdka

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 function openNav() {
     document.getElementById("myNav").style.width = "25%";
+    document.getElementById("myNav").style.height = "40%";
+        
 }
 
 /* Close when someone clicks on the "x" symbol inside the overlay */
 function closeNav() {
     document.getElementById("myNav").style.width = "0%";
+    document.getElementById("myNav").style.height = "0%";
 }
