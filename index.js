87d9af273b1589551696d549ba53f521d1912f44
Gold3n <marek@pavlicek.eu>
2021-12-01 16:38:04 +0100

    Dalsi

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+function openFilter() {
+    document.getElementById("myFilter").style.width = "350px";
+    document.getElementById("myFilter").style.height = "250px";
+        
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeFilter() {
+    document.getElementById("myFilter").style.width = "0px";
+    document.getElementById("myFilter").style.height = "50px";
+}
 function openNav() {
     document.getElementById("myNav").style.width = "350px";
     document.getElementById("myNav").style.height = "250px";
