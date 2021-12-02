b9257eb01040c16e27fbb9c0988dbc9c98ca78d6
Gold3n <marek@pavlicek.eu>
2021-12-02 11:22:52 +0100

    💀

--- index.js
+++ index.js
@@ -1,14 +1,3 @@
-function openFilter() {
-    document.getElementById("myFilter").style.width = "350px";
-    document.getElementById("myFilter").style.height = "250px";
-        
-}
-
-/* Close when someone clicks on the "x" symbol inside the overlay */
-function closeFilter() {
-    document.getElementById("myFilter").style.width = "0px";
-    document.getElementById("myFilter").style.height = "50px";
-}
 function openNav() {
     document.getElementById("myNav").style.width = "350px";
     document.getElementById("myNav").style.height = "250px";
@@ -18,5 +7,5 @@ function openNav() {
 /* Close when someone clicks on the "x" symbol inside the overlay */
 function closeNav() {
     document.getElementById("myNav").style.width = "0px";
-    document.getElementById("myNav").style.height = "50px";
+    document.getElementById("myNav").style.height = "100px";
 }
