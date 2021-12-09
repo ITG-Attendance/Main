dc92ec21271bc8f8fa3622aa343f3ce3907fe4c7
Gold3n <marek@pavlicek.eu>
2021-12-09 10:52:23 +0100

    YEEEEEEEEEEE

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function openNav() {
-    document.getElementById("myNav").style.width = "350px";
+function openNav1() {
+    Close();
+    setTimeout(() => {  document.getElementById("myNav").style.width = "350px";
     document.getElementById("myNav").style.height = "250px";
+    var element = document.getElementById("Option1");
+    element.classList.add("MojeFunkce");  }, 500);
+       
+}
+function openNav2() {
+    Close();
+    setTimeout(() => {document.getElementById("myNav").style.width = "350px";
+    document.getElementById("myNav").style.height = "250px";
+    var element = document.getElementById("Option2");
+    element.classList.add("MojeFunkce");}, 500);
         
 }
-
+function Close(){
+    closeNav1();
+    closeNav2();
+}
 /* Close when someone clicks on the "x" symbol inside the overlay */
-function closeNav() {
+function closeNav1() {
     document.getElementById("myNav").style.width = "0px";
     document.getElementById("myNav").style.height = "100px";
+    var element = document.getElementById("Option1");
+    element.classList.remove("MojeFunkce"); 
+
+}function closeNav2() {
+    document.getElementById("myNav").style.width = "0px";
+    document.getElementById("myNav").style.height = "100px";
+    var element = document.getElementById("Option2");
+    element.classList.remove("MojeFunkce"); 
 }
